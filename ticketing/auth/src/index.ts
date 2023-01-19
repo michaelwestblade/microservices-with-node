c61f05ac209c0723800a9dc20464adfce2fb2007
Michael Westblade <michaelwestblade@gmail.com>
2023-01-18 20:46:20 -0600

    trying to get ingress working

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 const port = process.env.port || 3000;
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
