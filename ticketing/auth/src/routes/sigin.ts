7503030d93e04774446ab0d3a69d8b2fac9564e7
Michael Westblade <michaelwestblade@gmail.com>
2023-03-06 20:51:54 -0600

    moved request validation to a middleware

--- ticketing/auth/src/routes/sigin.ts
+++ ticketing/auth/src/routes/sigin.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body } from 'express-validator';
+import {validateRequest} from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello, User')
+router.post('/api/users/signin', [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Password is required'),
+], validateRequest, (req: Request, res: Response) => {
+  res.send('OK');
 });
 
 
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, {Request, Response} from 'express';
-import { body, validationResult } from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { body } from 'express-validator';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -13,13 +12,7 @@ const JWT_KEY = process.env.JWT_KEY || '';
 router.post('/api/users/signup', [
   body('email').isEmail().withMessage('email must be valid'),
   body('password').trim().isLength({min: 4, max: 20}).withMessage('password must be between 4 and 20 characters')
-],async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array());
-  }
-
+], validateRequest, async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
   const existingUser = await User.findOne({email});
