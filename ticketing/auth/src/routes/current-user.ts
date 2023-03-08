d967ffa699a4b8045d02d43a013ca56c36c2b987
Michael Westblade <michaelwestblade@gmail.com>
2023-03-07 20:39:29 -0600

    get current user

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
+const JWT_KEY = process.env.JWT_KEY || '';
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello, User')
+  if (!req?.session?.jwt) {
+    return res.send({currentUser: null});
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, JWT_KEY);
+
+    return res.send({currentUser: payload});
+  } catch ( error ) {
+    console.log(error);
+    return res.send({currentUser: null});
+  }
 });
 
 export {router as currentUserRouter};
