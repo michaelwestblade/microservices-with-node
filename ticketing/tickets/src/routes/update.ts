dbd9dc3bffaca0d75a501e9df81908fa84493410
Michael Westblade <michaelwestblade@gmail.com>
2023-03-23 19:47:05 -0500

    exposed the api

--- ticketing/tickets/src/routes/update.ts
+++ ticketing/tickets/src/routes/update.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { Ticket } from "../models/ticket";
 import {
+  currentUser,
   NotAuthorizedError,
   NotFoundError,
   requireAuth,
@@ -26,10 +27,14 @@ router.put(
     const ticket = await Ticket.findById(request.params.id);
 
     if (!ticket) {
+      console.log(`No ticket found for id ${request.params.id}`);
       throw new NotFoundError();
     }
 
     if (ticket.userId !== request?.currentUser?.id) {
+      console.log(
+        `User ${request?.currentUser?.id} does not match id on ticket ${ticket.userId}`
+      );
       throw new NotAuthorizedError();
     }
 
