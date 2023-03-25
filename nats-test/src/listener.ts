94d748b3567ede3a82761360538a7f76a5980f62
Michael Westblade <michaelwestblade@gmail.com>
2023-03-25 16:11:38 -0500

    testing nats streaming server

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,24 @@
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
+console.clear();
+
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("listener connected to nats");
+
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", (message: Message) => {
+    const data = message.getData();
+
+    if (typeof data === "string") {
+      console.log(
+        `Received event #${message.getSequence()}, with data: ${data}`
+      );
+    }
+  });
+});
