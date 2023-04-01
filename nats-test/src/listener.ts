4eeb59b047889613c15b7773be9f8e162c7bb350
Michael Westblade <michaelwestblade@gmail.com>
2023-04-01 12:39:47 -0500

    abstract listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,4 +1,4 @@
-import nats, { Message } from "node-nats-streaming";
+import nats, { Message, Stan } from "node-nats-streaming";
 import { randomBytes } from "crypto";
 
 console.clear();
@@ -10,6 +10,12 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
   console.log("listener connected to nats");
 
+  stan.on("close", () => {
+    console.log("NATS connection closed");
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
   const subscription = stan.subscribe(
     "ticket:created",
     "orders-service-queue-group"
@@ -23,5 +29,55 @@ stan.on("connect", () => {
         `Received event #${message.getSequence()}, with data: ${data}`
       );
     }
+
+    message.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage: (data: any, message: Message) => void;
+  private client: Stan;
+  protected ackWait: number = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", (message: Message) => {
+      console.log(
+        `Message Received: ${message.getSubject()} / ${this.queueGroupName}`
+      );
+
+      const parsedData = this.parseMessage(message);
+      this.onMessage(parsedData, message);
+    });
+  }
+
+  parseMessage(message: Message) {
+    const data = message.getData();
+    return typeof data === "string"
+      ? JSON.parse(data)
+      : JSON.parse(data.toString("utf-8"));
+  }
+}
