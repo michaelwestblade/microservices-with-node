51a4ebc0d094f3a8487c07aefc6d08bec2be2c56
Michael Westblade <michaelwestblade@gmail.com>
2023-04-01 13:34:41 -0500

    promisified the publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -6,16 +7,17 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("publisher connected to nats");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("ticket:created published");
-  });
+  const ticketCreatedPublisher = new TicketCreatedPublisher(stan);
+  try {
+    await ticketCreatedPublisher.publish({
+      id: "123",
+      title: "concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
