528df2029f7fbe88205f7d4f6fdf23a3bc2118e2
Michael Westblade <michaelwestblade@gmail.com>
2023-04-04 20:22:57 -0500

    starting to build orders model

---
+++ ticketing/orders/src/models/order.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties that
+// are required to create a new ticket
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties that a User document has
+interface OrderDocument extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties a user model has
+interface OrderModel extends mongoose.Model<any> {
+  build: (attrs: OrderAttrs) => OrderDocument;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+      required: false,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => new Order(attrs);
+
+const Order = mongoose.model<OrderDocument, OrderModel>("Order", orderSchema);
+
+export { Order };
