fd7828a25ad3c4f0f258aa12872a649980f9976e
Michael Westblade <michaelwestblade@gmail.com>
2023-04-05 20:53:37 -0500

    new order tests

---
+++ ticketing/orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@westbladetickets/common";
+
+describe("New Order Routes tests", () => {
+  it("returns an error if the ticket does not exist", async () => {
+    const cookie = global.signin();
+    const ticketId = new mongoose.Types.ObjectId();
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", cookie)
+      .send({ ticketId })
+      .expect(404);
+  });
+
+  it("returns an error if the ticket is already reserved", async () => {
+    const cookie = global.signin();
+    const ticket = Ticket.build({
+      title: "test",
+      price: 20,
+    });
+    await ticket.save();
+
+    const order = Order.build({
+      ticket,
+      userId: "randomId",
+      status: OrderStatus.CREATED,
+      expiresAt: new Date(),
+    });
+    await order.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", cookie)
+      .send({ ticketId: ticket.id })
+      .expect(400);
+  });
+
+  it("reserves a ticket", async () => {
+    const cookie = global.signin();
+    const ticket = Ticket.build({
+      title: "test",
+      price: 20,
+    });
+    await ticket.save();
+
+    await request(app)
+      .post("/api/orders")
+      .set("Cookie", cookie)
+      .send({ ticketId: ticket.id })
+      .expect(201);
+  });
+});
