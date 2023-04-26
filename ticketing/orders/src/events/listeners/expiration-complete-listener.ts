df3d5134aac6ff0546a0876d9abc647b076c25cb
Michael Westblade <michaelwestblade@gmail.com>
2023-04-25 20:26:47 -0500

    publishing order cancelled

--- ticketing/orders/src/events/listeners/expiration-complete-listener.ts
+++ ticketing/orders/src/events/listeners/expiration-complete-listener.ts
@@ -8,13 +8,14 @@ import {
 import { queueGroupName } from "./queue-group-name";
 import { Message } from "node-nats-streaming";
 import { Order } from "../../models/order";
+import { OrderCancelledPublisher } from "../publishers/order-cancelled-publisher";
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.EXPIRATION_COMPLETE;
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent["data"], message: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).populate("ticket");
 
     if (!order) {
       throw new Error(`No order found for orderId ${data.orderId}`);
@@ -24,6 +25,13 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       status: OrderStatus.CANCELLED,
     });
     await order.save();
+
+    const orderCancelledPublisher = new OrderCancelledPublisher(this.client);
+    await orderCancelledPublisher.publish({
+      id: data.orderId,
+      version: order.version,
+      ticket: { id: order.ticket.id },
+    });
 
     message.ack();
   }
