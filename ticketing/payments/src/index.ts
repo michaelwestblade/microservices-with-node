14f595d436d003122bd3e9d3a72cf818d1266b8e
Michael Westblade <michaelwestblade@gmail.com>
2023-04-26 20:43:59 -0500

    skaffolding payments service

---
+++ ticketing/payments/src/index.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const port = process.env.port || 3000;
+const MONGO_URI = process.env.MONGO_URI || "";
+const NATS_URL = process.env.NATS_URL || "";
+const NATS_CLIENT_ID = process.env.NATS_CLIENT_ID || "";
+const NATS_CLUSTER_ID = process.env.NATS_CLUSTER_ID || "";
+
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error(error);
+  }
+
+  try {
+    await natsWrapper.connect(NATS_CLUSTER_ID, NATS_CLIENT_ID, NATS_URL);
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+  });
+};
+
+start();
---
+++ ticketing/payments/src/test/setup.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+declare global {
+  var signin: () => string[];
+}
+
+const JWT_KEY = process.env.JWT_KEY || "";
+
+global.signin = () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  // Build a JWT payload. { id, email }
+  const payload = { id, email: "test@test.com" };
+
+  // Create the JWT
+  const token = jwt.sign(payload, JWT_KEY);
+
+  // Build session Object {}
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJson = JSON.stringify(session);
+
+  // Take JSON and encode as base64
+  const base64 = Buffer.from(sessionJson).toString("base64");
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
+
+let mongo: MongoMemoryServer;
+
+jest.mock("../nats-wrapper");
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
